9aa4833c8ed88d519de7dc373b412917dfc89481
Jeff37 <jfgodeau@gmail.com>
2015-07-28 01:47:09 +0200

    first commit

---
+++ 20150721_Apercu-GPX_Complet.R
@@ -0,0 +1,98 @@
+rm(list=ls())
+library(RgoogleMaps)
+library(rgdal)
+library(knitr)
+library(leaflet)#
+
+# The function 'reverseGeoCode' is taken from J. Aswani post on http://allthingsr.blogspot.com/2012/03/geocode-and-reverse-geocode-your-data.html
+# Ref: J. Aswani, “All Things R: Geocode and reverse geocode your data using, R, JSON and Google Maps’ Geocoding API,” All Things R, 20-Mar-2012. .
+reverseGeoCode <- function(latlng) {
+  latlngStr <-  gsub(' ','%20', paste(latlng, collapse=","))#Collapse and Encode URL Parameters
+  library("RJSONIO") #Load Library
+  #Open Connection
+  connectStr <- paste('http://maps.google.com/maps/api/geocode/json?sensor=false&latlng=',latlngStr, sep="")
+  con <- url(connectStr)
+  data.json <- fromJSON(paste(readLines(con), collapse=""))
+  close(con)
+  #Flatten the received JSON
+  data.json <- unlist(data.json)
+  if(data.json["status"]=="OK")
+    address <- data.json["results.formatted_address"]
+  return (address)
+}
+
+#### The function to call GPX_Overview
+## * If "GPXname=NULL" the .gpx files processed are those which are not yet as .png overview (the names are collected in 'AF')
+## One or more .gpx file names can be provided as text vector
+## * "option" should be specified wheter to output only the .png map, the html report (with leaflet interactive map) or both
+GPX_Overview <- function(GPXname=NULL,option=c("SimplePNG","htmlReport","both")){
+  #################################################################################
+  ### Overview of the content of a .gpx (layer = track) on a googlemaps background
+  setwd("/home/jf/Dropbox/_Carto⁄LIFE-ELIA[dropBox]/Fichiers GPX/GPX2PNG") ### On my laptop
+  ## List out .pgx files which were not yet processed
+  GPXfile.list <- list.files()[1:(length(list.files())-2)]
+  PNGfile.list <- list.files(paste(getwd(),"/OutputPNG",sep=""))
+  comp <- substring(PNGfile.list[-length(PNGfile.list)],1,25)
+  AF <- GPXfile.list[is.na(match(GPXfile.list,comp))]
+  if(!is.null(GPXname)){AF <- GPXname} ## Choose .gpx files to process
+  ### sapply the script on each .gpx file
+  sapply(AF,function(GPXfile) {filename <<- GPXfile
+  pts <<-  readOGR(GPXfile, layer = "track_points")
+  ## Keep the coordinates with 'native' CRS (WGS84) in decimal degrees
+  crd.gpx <<- data.frame(pts@coords)
+  colnames(crd.gpx) <<- c("Long", "Lat")
+  ## Reproject on metric system
+  pts.crd <<- spTransform(pts,CRS("+init=epsg:3035"))@coords ### EUROPE
+  ### Produce map using RGoogleMaps library
+  bb <<- qbbox(pts@bbox[2,],pts@bbox[1,], TYPE = "all",
+               margin = list(m = rep(1, 4),TYPE = c("perc", "abs")[1]))
+  range.lat <- 3*(bb$latR[2]-bb$latR[1])
+  range.lon <- 3*(bb$lonR[2]-bb$lonR[1])
+  bb.w <<- list(latR=c(bb$latR[1]-range.lat,bb$latR[2]+range.lat)
+                ,lonR=c(bb$lonR[1]-range.lon,bb$lonR[2]+range.lon))
+  ### Create MyMap and export as .png background before plotting track points
+  MyMap <<- GetMap.bbox(bb$lonR, bb$latR, destfile = paste(getwd(),"/OutputPNG/FondCarte/FondCarteGPX-",GPXfile,".png",sep="")
+                        , maptype = "hybrid",size = c(640, 640))
+  Ncut <<- as.integer(sqrt(length(pts)))
+  COL <<- cut(1:length(pts),Ncut,labels=heat.colors(Ncut))
+  ### Call .Rmd for knitting into .html
+  if(option=="htmlReport" | option=="both"){
+    MyMapWideHyb <<- GetMap.bbox(bb.w$lonR, bb.w$latR, destfile =
+                                   paste(getwd(),"/OutputPNG/FondCarte/FondCarteGPX-",GPXfile,"Hyb_WIDE.png",sep="")
+                                 , maptype = "hybrid",size = c(640, 640))
+    MyMapWideTer <<- GetMap.bbox(bb.w$lonR, bb.w$latR, destfile = paste(getwd(),"/OutputPNG/FondCarte/FondCarteGPX-",GPXfile,"Ter_WIDE.png",sep="")
+                                 , maptype = "terrain",size = c(640, 640))
+    
+    setwd('/home/jf/Dropbox (CARAH)/_Carto⁄LIFE-ELIA[dropBox]/Fichiers GPX/GPX2PNG/Output_html/')
+    # Note!!: knit2html is not working with leaflet widget creation. The widget is not visible in the html page, only the message "!–html_preserve–"
+    ### #knit2html(input='/home/jf/Dropbox (CARAH)/_Carto⁄LIFE-ELIA[dropBox]/Fichiers GPX/Apercu-GPX-Full_2html.Rmd',output=GPXfile,envir=globalenv()) 
+    ## The solution is explained here: http://stackoverflow.com/questions/28585238/r-rendering-html-widget-in-external-browser
+    rmarkdown::render(input='/home/jf/Dropbox (CARAH)/_Carto⁄LIFE-ELIA[dropBox]/Fichiers GPX/Apercu-GPX-Full_2html.Rmd'
+                      ,output_dir='.'
+                      ,output_file=paste(GPXfile,'.html',sep="")
+                      ,envir=globalenv())
+    setwd("/home/jf/Dropbox/_Carto⁄LIFE-ELIA[dropBox]/Fichiers GPX/GPX2PNG")
+  }
+  ### Find the first identified address to get country name (used in naming the .png output)
+  ads.beg <- NULL;nbeg <- 1
+  while(!is.character(ads.beg)){ads.beg <- try(reverseGeoCode(crd.gpx[nbeg,2:1]))
+  nbeg <- nbeg+1}
+  SpltAds <- unlist(strsplit(ads.beg,", "))
+  CNTRY <- SpltAds[length(SpltAds)]
+  # Simple .png output of the track on google hybrid background
+  if(option=="SimplePNG" | option=="both"){
+    png(paste(getwd(),"/OutputPNG/",GPXfile,"_",CNTRY, ".png",sep="")
+        ,width=800,height=800)
+    PlotOnStaticMap(MyMap, lat = crd.gpx$Lat, lon = crd.gpx$Long, size = c(640,640),
+                    cex = 1, pch = 20, col = as.character(COL), add = F)
+    dev.off()}
+  })
+}
+
+#Files2View <- c("2014-08-27 @ 18-16-37.gpx","2014-09-01 @ 23-10-49.gpx") # Some names
+
+GPX_Overview(option="both")
+GPX_Overview(option="htmlReport")
+GPX_Overview(GPXname="2014-06-05 @ 10-36-14.gpx",option="htmlReport")
+GPX_Overview(GPXname=Files2View,option="both")
+
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# GPX_Overview
+An R script that produces maps of an entire .gpx file, including an interactive leaflet map, and compute the speed between points
+
